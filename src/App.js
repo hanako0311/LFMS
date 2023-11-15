fab6e98c9e0494169bf60d82caef69b5467acd1e
nixxxxe <denissseavila21@gmail.com>
2023-11-16 00:48:27 +0800

    Login/Register

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
+// App.js
 import React from 'react';
-import './App.css'; 
-import backgroundImage from './landingbg.png'; 
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import './App.css';
+import backgroundImage from './landingbg.png';
+import Registration from './Register';
+import Login from './Login'; // Import the Login component
 
-const App = () => {
+const LandingPage = () => {
   return (
     <div className="landing-page" style={{ backgroundImage: `url(${backgroundImage})` }}>
       <div className="header">
         <div>
-          <button className="login-button">Login</button>
-          <button className="register-button">Register</button>
+          <Link to="/login">
+            <button className="login-button">Login</button>
+          </Link>
+          <Link to="/register">
+            <button className="register-button">Register</button>
+          </Link>
         </div>
       </div>
       <div className="content">
         <h1 className="welcome-text">
-        <span className="italic-text">Welcome to</span> <br /> 
-        <span className="lf-text">LOST AND FOUND</span> <br />
-        <span className="italic-text">  Find Lost Items, Report Found Items </span>
+          <span className="italic-text">Welcome to</span> <br />
+          <span className="lf-text">LOST AND FOUND</span> <br />
+          <span className="italic-text">Find Lost Items, Report Found Items</span>
         </h1>
       </div>
     </div>
+  );
+};
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/register" element={<Registration />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   );
 };
 
---
+++ src/Register.js
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import backgroundImage from './registerbg.png';
+import './App.css';
+
+const Register = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    firstName: '',
+    lastName: '',
+    password: ''
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle the form submission logic here
+    console.log('Form data submitted:', formData);
+    alert('Registration Complete'); // Show a popup message
+    setFormData({ // Reset the form fields
+      email: '',
+      firstName: '',
+      lastName: '',
+      password: ''
+    });
+  };
+
+  return (
+    <div className="register-page">
+      <div className="register-container">
+        <form className="register-form" onSubmit={handleSubmit}>
+          <h2>Register</h2>
+          <input
+            type="email"
+            name="email"
+            placeholder="Enter email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+          <input
+            type="text"
+            name="firstName"
+            placeholder="Enter First Name"
+            value={formData.firstName}
+            onChange={handleChange}
+            required
+          />
+          <input
+            type="text"
+            name="lastName"
+            placeholder="Enter Last Name"
+            value={formData.lastName}
+            onChange={handleChange}
+            required
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Enter password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+          <button type="submit">Register</button>
+          <p className="login-link">
+            Already have an account? <Link to="/login">Login now</Link>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
