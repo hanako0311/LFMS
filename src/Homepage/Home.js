c9ae594dc3fc98567798d6ca02137f07d96e95b3
Hana <hanako0311>
2023-11-21 16:23:47 +0800

    Fixed

--- src/Homepage/Home.js
+++ src/Homepage/Home.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import './home.css'; // Import CSS file
 
-
 function Home() {
-    
     return (
         <div className="main-container">
             <div className="top-bar">
@@ -15,80 +13,30 @@ function Home() {
                     </div>
                 </div>
             </div>
-          </div>
-          <div className="search-box">
-            <div className="search-input">
-              <div className="search-icon">
-                <div className="search-icon-inner" />
-              </div>
-              <div className="search-text">Search here...</div>
-            </div>
-          </div>
-          <div className="content-section">
-            <div className="found-items">Found Items</div>
-            <div className="items-count">15</div>
-          </div>
-          <div className="dashboard-section">
-            <div className="dashboard-title">Dashboard</div>
-<<<<<<< HEAD
-
-             {/* New Search Bar Section */}
-             <div className="search-bar-container">
-                <div className="search-bar">
-                    <div className="search-icon-container">
-                        <div className="search-icon"></div>
+            <div className="search-box">
+                <div className="search-input">
+                    <div className="search-icon">
+                        <div className="search-icon-inner" />
                     </div>
                     <div className="search-text">Search here...</div>
                 </div>
             </div>
-           
-            {/* Found Items Section */}
-            <div className="found-items-section">
-                <div className="found-items-box">
-                    <div className="found-items-header">
-                        <div className="found-items-title">Found Items</div>
-                        <div className="found-items-count">15</div>
-                    </div>
-                </div>
+            <div className="content-section">
+                <div className="found-items">Found Items</div>
+                <div className="items-count">15</div>
             </div>
-
-            {/* Lost Items Section */}
+            <div className="dashboard-section">
+                <div className="dashboard-title">Dashboard</div>
+            </div>
             <div className="lost-items-section">
-                <div className="lost-items-box">
-                    <div className="lost-items-header">
-                        <div className="lost-items-title">Lost Items</div>
-                        <div className="lost-items-count">34</div>
-                    </div>
-                </div>
+                <div className="lost-items-title">Lost Items</div>
+                <div className="lost-items-count">34</div>
             </div>
-
-             {/* Sidebar Section */}
-             <div className="sidebar">
-                <div className="sidebar-btn" style={{ top: '242px' }}></div>
-                <div className="sidebar-btn" style={{ top: '353px' }}></div>
-                <div className="sidebar-btn" style={{ top: '490px' }}></div>
-                <div className="sidebar-btn" style={{ top: '847px' }}></div>
+            <div className="image-section">
+                <img className="placeholder-image" src="https://via.placeholder.com/512x512" alt="Placeholder" />
             </div>
-
-            {/* More JSX elements go here following the same pattern */}
-            {/* ... */}
         </div>
-
-        
     );
-}
-=======
-          </div>
-          <div className="lost-items-section">
-            <div className="lost-items-title">Lost Items</div>
-            <div className="lost-items-count">34</div>
-          </div>
-          <div className="image-section">
-            <img className="placeholder-image" src="https://via.placeholder.com/512x512" alt="Placeholder" />
-          </div>
-        </div>
-      );
-    };
->>>>>>> 70cfec458503bba3e46441ffe4273357acc2365f
+};
 
-export default Home; 
+export default Home;
