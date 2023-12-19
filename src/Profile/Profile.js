e688c83beab684eaa907116434a2d96d21335749
klod <acbabatid7@gmail.com>
2023-12-19 20:33:15 +0800

    initial profile

--- src/Profile/Profile.js
+++ src/Profile/Profile.js
@@ -1,69 +1,116 @@
+import { Button } from "@mui/material";
+import { useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 import "./profile.css";
 
 const Profile = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-   /* const onHomePageClick = useCallback(() => {
-        navigate("/home"); 
-      }, [navigate]);
-    
-      const onReportLostClick = useCallback(() => {
-        navigate("/report-lost-items"); 
-      }, [navigate]);
-    const onReportFoundClick = useCallback(() => {
-        navigate("/report-found-items"); 
-      }, [navigate]);
-    
-      const onSignOutClick = useCallback(() => {
-        navigate("/"); 
-      }, [navigate]); */
+  const onReportLostContainerClick = useCallback(() => {
+    // Please sync "Report Lost Items" to the project
+  }, []);
+
+  const onReportFoundClick = useCallback(() => {
+    // Please sync "Report Found Items" to the project
+  }, []);
+
+  const onProfileClick = useCallback(() => {
+    navigate("/");
+  }, [navigate]);
+
+  const onHomeClick = useCallback(() => {
+    // Please sync "Dashboard Found Items - Default" to the project
+  }, []);
+
+  const onImage5Click = useCallback(() => {
+    // Please sync "History" to the project
+  }, []);
 
   return (
     <div className="profile">
-      <img className="top-menu-icon" alt="" src="/top-menu.svg" />
-      <img className="report-found-icon" alt="" src="/report-found.svg" />
-      <div className="report-lost">
-        <img className="group-icon" alt="" src="/group.svg" />
-        <div className="search-dollar-search-pay-prod">
-          <img
-            className="search-dollar-search-pay-prod-icon"
-            alt=""
-            src="/searchdollarsearchpayproductcurrencyquerymagnifyingcashbusinessmoneyglass.svg"
-          />
-          <b className="b">?</b>
-        </div>
-      </div>
-      <div className="top-menu">
-        <div className="bg" />
-      </div>
-      <img className="lf-splash-5" alt="" src="/lf-splash-5@2x.png" />
-      <div className="search-bar">
-        <div className="content">
-          <img className="magnifier-icon" alt="" src="/magnifier.svg" />
-          <div className="search-here">Search here...</div>
-        </div>
-      </div>
       <div className="card">
         <div className="background" />
-        <div className="button">
-          <div className="button-child" />
-          <div className="edit-profile">EDIT PROFILE</div>
+        <Button
+          className="editprofilebutton"
+          sx={{ width: 218.7190399169922 }}
+          color="primary"
+          variant="contained"
+        >
+          EDIT PROFILE
+        </Button>
+        <div className="address">
+          <div className="address1">Address</div>
+          <div className="skina-ubec-ytic">Skina, Ubec Ytic</div>
         </div>
-        <div className="info">
-          <div className="name">Name</div>
-          <div className="john-doe">John Doe</div>
+        <div className="email">
+          <div className="address1">Email</div>
+          <div className="skina-ubec-ytic">zara.w@schl.edu</div>
         </div>
-        <div className="info1">
-          <div className="name">Email</div>
-          <div className="johndoegmailcom">johndoe@gmail.com</div>
+        <div className="name">
+          <div className="address1">Name</div>
+          <div className="zara-wardani">Zara Wardani</div>
         </div>
-        <div className="info2">
-          <div className="name">Address</div>
-          <div className="johndoegmailcom">Skina, Ubec Ytic</div>
-        </div>
+        <img className="picture-icon" alt="" src="/picture@2x.png" />
       </div>
-      <img className="profile-child" alt="" src="/group-34.svg" />
+      <div className="top-menu">
+        <div className="side-menu">
+          <div className="bg" />
+          <img className="side-menu-child" alt="" src="/line@2x.png" />
+          <div className="nav">
+            <div className="report-lost" onClick={onReportLostContainerClick}>
+              <img className="group-icon" alt="" src="/group@2x.png" />
+              <div className="search-dollar-search-pay-prod">
+                <img
+                  className="search-dollar-search-pay-prod-icon"
+                  alt=""
+                  src="/searchdollarsearchpayproductcurrencyquerymagnifyingcashbusinessmoneyglass@2x.png"
+                />
+                <b className="b">?</b>
+              </div>
+            </div>
+            <img
+              className="report-found-icon"
+              alt=""
+              src="/report-found@2x.png"
+              onClick={onReportFoundClick}
+            />
+            <img
+              className="profile-icon"
+              alt=""
+              src="/profile@2x.png"
+              onClick={onProfileClick}
+            />
+            <img
+              className="home-icon"
+              alt=""
+              src="/home@2x.png"
+              onClick={onHomeClick}
+            />
+            <img
+              className="image-5-icon"
+              alt=""
+              src="/image-5@2x.png"
+              onClick={onImage5Click}
+            />
+          </div>
+          <img className="subtract-icon" alt="" src="/subtract@2x.png" />
+        </div>
+        <div className="bg1" />
+        <div className="search-bar">
+          <div className="content">
+            <img className="magnifier-icon" alt="" src="/magnifier@2x.png" />
+            <div className="search-here">Search here...</div>
+          </div>
+        </div>
+        <div className="user">
+          <img className="placeholder-icon" alt="" src="/placeholder@2x.png" />
+          <div className="name2">
+            <b className="zara-wardani1">Zara Wardani</b>
+            <div className="admin">Admin</div>
+          </div>
+        </div>
+        <img className="lf-splash-1" alt="" src="/lf-splash-1@2x.png" />
+      </div>
     </div>
   );
 };
