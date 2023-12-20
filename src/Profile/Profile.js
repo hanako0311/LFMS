56a6b54a3f9e0d1b5c6e852e688d38b116e24697
klod <acbabatid7@gmail.com>
2023-12-20 19:33:45 +0800

    profile ok naa

--- src/Profile/Profile.js
+++ src/Profile/Profile.js
@@ -6,7 +6,11 @@ import "./profile.css";
 const Profile = () => {
   const navigate = useNavigate();
   const [userDetails, setUserDetails] = useState(null);
-  
+  const [isEditMode, setIsEditMode] = useState(false);
+  const [editedFirstName, setEditedFirstName] = useState('');
+  const [editedLastName, setEditedLastName] = useState('');
+
+
   useEffect(() => {
     // Fetch user details when the component mounts
     fetchUserDetails();
@@ -61,6 +65,49 @@ const Profile = () => {
     
   }, []);
 
+  const toggleEditMode = () => {
+    setIsEditMode(!isEditMode);
+    // Set the edited values to the current user details when entering edit mode
+    if (!isEditMode) {
+        setEditedFirstName(userDetails?.firstname || '');
+        setEditedLastName(userDetails?.lastname || '');
+    }
+  };
+
+  const userId = userDetails?.uid;
+  const handleSaveProfile = async () => {
+    try {
+        const email = window.sessionStorage.getItem("user");
+        const response = await fetch(`http://localhost:8080/users/${userId}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                firstname: editedFirstName,
+                lastname: editedLastName,
+            }),
+        });
+
+        if (response.ok) {
+            // Update local state with saved changes
+            setUserDetails({
+                ...userDetails,
+                firstname: editedFirstName,
+                lastname: editedLastName,
+            });
+
+            // Exit edit mode
+            setIsEditMode(false);
+        } else {
+            console.error('Failed to save profile changes');
+        }
+    } catch (error) {
+        console.error('Error saving profile changes:', error);
+    }
+  };
+
+
   return (
     <div className="profilepage">
       <div className="card">
@@ -70,8 +117,9 @@ const Profile = () => {
           sx={{ width: 218.7190399169922 }}
           color="primary"
           variant="contained"
+          onClick={isEditMode ? handleSaveProfile : toggleEditMode}
         >
-          EDIT PROFILE
+          {isEditMode ? 'SAVE' : 'EDIT PROFILE'}
         </Button>
         <div className="email">
           <div className="label">Email</div>
@@ -79,11 +127,27 @@ const Profile = () => {
         </div>
         <div className="lname">
           <div className="label">Last Name</div>
-          <div className="data">{userDetails?.lastname || 'Loading...'}</div>
+            {isEditMode ? (
+              <input
+                type="text"
+                value={editedLastName}
+                onChange={(e) => setEditedLastName(e.target.value)}
+                />
+              ) : (
+            <div className="data">{userDetails?.lastname || 'Loading...'}</div>
+            )}
         </div>
         <div className="fname">
           <div className="label">First Name</div>
+            {isEditMode ? (
+              <input
+               type="text"
+               value={editedFirstName}
+               onChange={(e) => setEditedFirstName(e.target.value)}
+               />
+            ):(
           <div className="data">{userDetails?.firstname || 'Loading...'}</div>
+            )}
         </div>
         <img className="picture-icon" alt="" src="/picture@2x.png" />
       </div>
